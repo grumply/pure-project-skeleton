8e42ed286783a30f782ea8658f14383c7c749d5a
grumply <sean@grump.ly>
2018-03-05 15:52:17 -0600

    Add simple WebSocket examples. Refine default language pragmas. Fix up package.json scripts.

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -1,10 +1,44 @@
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 module Main where
 
 import Pure.Server
 import Pure.Connection
+import Pure.WebSocket
 
 import Lib
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = run (app "127.0.0.1" 8000)
 
+app ip port = Server {..}
+  where
+    key   = "server"
+    build = return
+    prime = return ()
+    connection = conn
+
+conn _ = Connection {..}
+  where
+    build = return
+    prime = void $ do
+      enact backendImpl
+      setExhaustLimit maxBound
+
+backendImpl = Impl backendAPI msgs reqs
+  where
+    msgs =
+          handleSayHello <:>
+          none
+    reqs =
+          handleAskTime <:>
+          none
+
+-- Note that if this server is running via `npm start`, you will not see "Got Hello!"
+handleSayHello = accepts sayHello $ \_done q ->
+  lift_ $ for q $ \_req ->
+    liftIO $ putStrLn "Got Hello!"
+
+handleAskTime = responds askTime $ \_done q ->
+  lift_ $ for q $ \(rsp,(_rqId,_req)) -> do
+    now <- millis
+    rsp (Right now)
--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -1,11 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, NoMonomorphismRestriction #-}
 module Main where
 
 import qualified Pure.App as App
 import Pure.View
+import Pure.Render
+
+import Lib
 
 main :: IO ()
-main = App.run App.App {..}
+main =
+  let ?serverIp = "127.0.0.1"
+      ?serverPort = 8000
+  in App.run app
+
+app = App.App {..}
   where
     key    = "MyApp"
     build  = return
@@ -16,10 +24,26 @@ main = App.run App.App {..}
 
 data MyAppRoute = HomeR deriving Eq
 
+data MyAppState ms = MyAppState Millis
+
 _MyApp = Controller {..}
   where
     key    = "MyApp"
     build  = return
     prime  = return ()
-    model  = Const ()
-    view _ = Div [] "Hello, World!"
+    model  = MyAppState 0
+    view (MyAppState (Millis ms)) =
+      Div [ ]
+        [ Button [ OnClick def sendHello ] "Say Hello"
+        , Br [] []
+        , Button [ OnClick def getTime ] "Get Time"
+        , Br [] []
+        , "Current Time: "
+        , Translated ms
+        ]
+      where
+        getTime _ = return $ Just $ void $
+          remote askTime () $ traverse_ (putModel . MyAppState)
+
+        sendHello _ = return $ Just $ void $
+          message sayHello ()
