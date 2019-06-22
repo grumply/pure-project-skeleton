2d52c3d3ca6b6d97f4e744b5cc4214d3ac83eed9
Sean Mackenzie <seanmhickman@gmail.com>
2019-06-21 23:06:54 -0500

    Switch to fetching master for pure-platform.

--- default.nix
+++ default.nix
@@ -1,12 +1,6 @@
 { nixpkgs ? import <nixpkgs> {} }:
 
-let inherit (nixpkgs) fetchFromGitHub;
-    pure-platform = import (fetchFromGitHub {
-      owner = "grumply";
-      repo = "pure-platform";
-      rev = "0cb3a8ba6fa0cb01b2c8958f9ba051bbef4065df";
-      sha256 = "1qw6yrc22kmzccnxzh0n40x3y3jw9sx79ygwwg3s3l3hgyljn5ks";
-    }) {};
+let pure-platform = import (builtins.fetchTarball https://github.com/grumply/pure-platform/tarball/master) {};
 
 in pure-platform.project ({ pkgs, ... }: {
   
