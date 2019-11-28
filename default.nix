0d04ecf74da079f500a20ad175a92599daad3923
Sean Hickman <sean@grumply.com>
2019-11-27 22:03:54 -0600

    Test the minimal flag compile time on travis.

--- default.nix
+++ default.nix
@@ -3,7 +3,9 @@
 let pure-platform = import (builtins.fetchTarball https://github.com/grumply/pure-platform/tarball/master) {};
 
 in pure-platform.project ({ pkgs, ... }: {
-  
+
+  minimal = true;
+
   packages = {
     backend = ./backend;
     shared = ./shared;
