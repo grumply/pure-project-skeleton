f77ff20c15484a77695487db080039bafd1d906e
Sean Hickman <sean@grumply.com>
2020-04-12 01:22:26 -0500

    Add a simple test example

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,7 +1,18 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import Pure.Test
 
-main :: IO ()
-main = print "Shouldn't see this."
+main :: IO (Maybe ())
+main = run suite
 
+suite :: Test sync ()
+suite = tests
+  [ test_reverse
+  ]
+
+test_reverse :: Test sync ()
+test_reverse = expect (original == doubleReversed)
+  where
+    original = [1,2,3]
+    doubleReversed = reverse (reverse original)
